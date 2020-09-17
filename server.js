631848c70215cf9a932dcae35326dcb9ae209bed
Anto Fernando <anto@Antos-MacBook-Air.local>
2020-09-17 09:02:23 +0530

    Intro To MiddleWare

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const morgan = require('morgan')
 
 //Route Files
 const bootcamps = require('./routes/bootcamps')
@@ -9,6 +11,13 @@ dotenv.config({ path: 'config/config.env' });
 
 
 const app = express();
+
+
+//Logging MiddleWare and  Morgan
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  //app.use(logger);
+}
 
 
 //Mount Routes
