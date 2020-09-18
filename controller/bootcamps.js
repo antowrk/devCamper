2dca87bc281520d32ab3c6243a7d37be9453ac12
Anto Fernando <anto@Antos-MacBook-Air.local>
2020-09-18 19:48:13 +0530

    GET All and Single BOOTCAMPS

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -4,16 +4,37 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc      Get All Bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all Bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.find();
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 
 // @desc      Get A Single Bootcamps
 // @route     GET /api/v1/bootcamps/id
 // @access    Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show Single Bootcamps' });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });   
+        }
+
+        res.status(400).json({
+            success: true,
+            data: bootcamp
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 
