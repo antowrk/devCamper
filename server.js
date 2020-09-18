dd219ec0019b4a41417aeadb0f45f1aa426ff606
Anto Fernando <anto@Antos-MacBook-Air.local>
2020-09-18 19:22:26 +0530

    Initial MongoDb Setuo and Colors

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors')
 const logger = require('./middleware/logger');
-const morgan = require('morgan')
-
-//Route Files
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
 
 //Load env vars
 dotenv.config({ path: 'config/config.env' });
 
+//MongoDB Connection
+connectDB();
 
+//Route Files
+const bootcamps = require('./routes/bootcamps')
+
+
+//Instance Of Express
 const app = express();
 
 
@@ -28,7 +34,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 //Server
 const PORT = process.env.PORT
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server started in ${process.env.NODE_ENV} mode at PORT ${PORT}`)
+  console.log(`Server started in ${process.env.NODE_ENV} mode at PORT ${PORT}`.yellow.bold)
 );
+
+//UnhandledRejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold.underline);
+  //Close Server
+  server.close(() => process.exit(1));
+})
